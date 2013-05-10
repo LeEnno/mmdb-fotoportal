48d97b82ff2fbe893d6d10bdd2a001bed4279fbe
LeEnno <enricoschlag@googlemail.com>
2013-05-10 11:48:41 +0200

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MmdbFotoportal::Application.config.secret_token = '5d1c9ea4c676d556499e45c84956d659ae77a7bcf60fc32ff9c8fb405273a82e47f95c8e9fc99166cba86311ef6d42fdbc0578ae0a4bb55d497e069c50b7c1b7'
