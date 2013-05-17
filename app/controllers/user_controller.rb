81d5f7ca0c465ab1a0916aac1ab516f8eb72cd48
LeEnno <enricoschlag@googlemail.com>
2013-05-17 17:04:03 +0200

    skip login when authenticated + logout

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,25 +1,53 @@
 class UserController < ApplicationController
+
+  before_filter :redirect_if_authenticated, :only => [:login, :register]
+  
   def login
-    
-  end
+    return if params[:user].blank?
 
-  def register
-    if !params[:user].blank?
-      @user = User.new(params[:user])
+    @user = User.find_by_email_and_password(
+      params[:user][:email],
+      params[:user][:password]
+    )
 
-      if @user.valid?
-        @user.save
-        create_session_and_redirect
-      end
+    if @user.nil?
+      flash[:error] = 'falsche Zugangsdaten'
+    else
+      create_session_and_redirect
     end
   end
 
-  def logout
+
+  def register
+    return if params[:user].blank?
+    @user = User.new(params[:user])
+
+    if @user.valid?
+      @user.save
+      create_session_and_redirect
+    end
   end
 
+  
+  def logout
+    Session.delete(session[:id])
+  end
+
+
+  # PRIVATE METHODS
+  # ------------------------------------------------------------------------------  
   private
+
   def create_session_and_redirect
     session[:id] = Session.create(:user => @user).id
+    redirect_to_gallery
+  end
+
+  def redirect_to_gallery
     redirect_to gallery_path
+  end
+
+  def redirect_if_authenticated
+    redirect_to_gallery if @is_logged_in
   end
 end
