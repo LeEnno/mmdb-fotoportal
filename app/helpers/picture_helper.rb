b7978b91b316c3d91314ba3c8d0092ec3dd01462
LeEnno <enricoschlag@googlemail.com>
2013-07-12 23:13:14 +0200

    added picture helper

---
+++ app/helpers/picture_helper.rb
@@ -0,0 +1,12 @@
+module PhotoHelper
+
+  def list_horizontal_group(key, value, li_class = '')
+    content_tag :li, :class => ['control-group', li_class].join(' ') do
+      out = label_tag(nil, key, :class => 'control-label')
+      out += content_tag(:div, :class => 'controls') do
+        value
+      end
+    end
+  end
+
+end
